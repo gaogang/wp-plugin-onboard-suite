f2d37fda23f5cce60b5a8bd4b5c1b1122abb559e
Gang Gao <gaogang@gmail.com>
2021-01-24 23:59:26 +0000

    Added more options and external id creation

--- wp-plugin-onboard-suite.php
+++ wp-plugin-onboard-suite.php
@@ -14,11 +14,15 @@ function onboard_suite_settings_init() {
  
     add_settings_section(
         'onboard_suitecrm_section',
-        __( 'Onboarding SuiteCRM', 'wp-plugin-onboard-suite' ), 
+        __( 'SuiteCRM Api Access', 'wp-plugin-onboard-suite' ), 
         'onboard_suitecrm_section_callback',
         'onboard'
     );
  
+    /**
+     * The base url of the SuiteCRM apis
+     * @example https://www.yoursuitecrm.com/api
+     */
     add_settings_field(
         'onboard_suitecrm_field_urlbase', 
         __( 'Url Base', 'wp-plugin-onboard-suite' ),
@@ -30,6 +34,7 @@ function onboard_suite_settings_init() {
         )
     );
 
+    /** SuiteCRM client Id */
     add_settings_field(
         'onboard_suitecrm_field_client_id', 
         __( 'Client Id', 'wp-plugin-onboard-suite' ),
@@ -41,6 +46,7 @@ function onboard_suite_settings_init() {
         )
     );
 
+    /** SuiteCRM client secret */
     add_settings_field(
         'onboard_suitecrm_field_client_secret', 
         __( 'Client Secret', 'wp-plugin-onboard-suite' ),
@@ -51,12 +57,99 @@ function onboard_suite_settings_init() {
             'label_for' => 'onboard_suitecrm_field_client_secret'
         )
     );
+
+    /** Section for external Id module configuration. */
+    add_settings_section(
+        'onboard_suitecrm_external_id_section',
+        __( 'External Id Module Configuration', 'wp-plugin-onboard-suite' ), 
+        'onboard_suitecrm_external_id_section_callback',
+        'onboard'
+    );
+
+    /** Name of the external Id Module in SuiteCRM */
+    add_settings_field(
+        'onboard_suitecrm_field_external_id_module', 
+        __( 'Name of SutieCRM module to store user info in a 3rd party system', 'wp-plugin-onboard-suite' ),
+        'onboard_suitecrm_field_external_id_module_callback',
+        'onboard',
+        'onboard_suitecrm_external_id_section',
+        array(
+            'label_for' => 'onboard_suitecrm_field_external_id_module'
+        )
+    );
+
+    /** 
+     * Name of the external system
+     * @example Wordpress
+     */
+    add_settings_field(
+        'onboard_suitecrm_field_system_name', 
+        __( 'Name of the 3rd party system', 'wp-plugin-onboard-suite' ),
+        'onboard_suitecrm_field_system_name_callback',
+        'onboard',
+        'onboard_suitecrm_external_id_section',
+        array(
+            'label_for' => 'onboard_suitecrm_field_system_name'
+        )
+    );
+
+    /** Name of the attribute for external system in the external Id module */
+    add_settings_field(
+        'onboard_suitecrm_field_attribute_system_name', 
+        __( 'Attribute in your external Id module to store 3rd party system name', 'wp-plugin-onboard-suite' ),
+        'onboard_suitecrm_field_attribute_system_name_callback',
+        'onboard',
+        'onboard_suitecrm_external_id_section',
+        array(
+            'label_for' => 'onboard_suitecrm_field_attribute_system_name'
+        )
+    );
+
+    /**
+     * Name of the attribute for username in the external system in the external Id module 
+     */
+    add_settings_field(
+        'onboard_suitecrm_field_attribute_username', 
+        __( 'Attribute in you external Id module to store username in the 3rd party system', 'wp-plugin-onboard-suite' ),
+        'onboard_suitecrm_field_attribute_username_callback',
+        'onboard',
+        'onboard_suitecrm_external_id_section',
+        array(
+            'label_for' => 'onboard_suitecrm_field_attribute_username'
+        )
+    );
+
+    /**
+     * Name of the attribute for user's id in the external system
+     */
+    add_settings_field(
+        'onboard_suitecrm_field_attribute_user_id', 
+        __( 'Attribute in your external Id module to store user Id in the 3rd party system', 'wp-plugin-onboard-suite' ),
+        'onboard_suitecrm_field_attribute_user_id_callback',
+        'onboard',
+        'onboard_suitecrm_external_id_section',
+        array(
+            'label_for' => 'onboard_suitecrm_field_attribute_user_id'
+        )
+    );
+
+    /** Name of the attribute for the user's SuiteCRM contact Id */
+    add_settings_field(
+        'onboard_suitecrm_field_attribute_contact_id', 
+        __( 'Attribute in your external Id module to store the user\'s SutieCRM contact Id', 'wp-plugin-onboard-suite' ),
+        'onboard_suitecrm_field_attribute_contact_id_callback',
+        'onboard',
+        'onboard_suitecrm_external_id_section',
+        array(
+            'label_for' => 'onboard_suitecrm_field_attribute_contact_id'
+        )
+    );
 }
 
 function onboard_suitecrm_section_callback( $args ) {
-    ?>
-    <p id="<?php echo esc_attr( $args['id'] ); ?>"><?php esc_html_e( 'Follow the white rabbit.', 'wp-plugin-onboard-suite' ); ?></p>
-    <?php
+}
+
+function onboard_suitecrm_external_id_section_callback( $args ) {
 }
 
 function onboard_suitecrm_field_urlbase_callback( $args ) {
@@ -69,6 +162,30 @@ function onboard_suitecrm_field_client_id_callback( $args ) {
 
 function onboard_suitecrm_field_client_secret_callback( $args ) {
     echo field_callback('onboard_suitecrm_field_client_secret', $args['label_for']);
+}
+
+function onboard_suitecrm_field_external_id_module_callback( $args ) {
+    echo field_callback('onboard_suitecrm_field_external_id_module', $args['label_for']);
+}
+
+function onboard_suitecrm_field_system_name_callback( $args ) {
+    echo field_callback('onboard_suitecrm_field_system_name', $args['label_for']);
+}
+
+function onboard_suitecrm_field_attribute_system_name_callback( $args ) {
+    echo field_callback('onboard_suitecrm_field_attribute_system_name', $args['label_for']);
+}
+
+function onboard_suitecrm_field_attribute_username_callback( $args ) {
+    echo field_callback('onboard_suitecrm_field_attribute_username', $args['label_for']);
+}
+
+function onboard_suitecrm_field_attribute_user_id_callback( $args ) {
+    echo field_callback('onboard_suitecrm_field_attribute_user_id', $args['label_for']);
+}
+
+function onboard_suitecrm_field_attribute_contact_id_callback( $args ) {
+    echo field_callback('onboard_suitecrm_field_attribute_contact_id', $args['label_for']);
 }
 
 function field_callback($id, $optionName) {
@@ -154,9 +271,11 @@ function get_access_token($options) {
         $clientSecret = $options['onboard_suitecrm_field_client_secret'];
     }
 
-    if (empty($urlBase) || empty($clientId || empty($clientSecret)))
+    if (empty($urlBase) || 
+        empty($clientId || 
+        empty($clientSecret)))
     {
-        return 'empty something';
+        return '';
     }
 
     $body = array(
@@ -167,6 +286,7 @@ function get_access_token($options) {
 
 
     $url = sprintf('%s%s', $urlBase, '/access_token');
+
     $response = wp_remote_post($url, array(
         'body' => wp_json_encode($body),
         'headers' => array(
@@ -175,7 +295,7 @@ function get_access_token($options) {
     ));
 
     if (is_wp_error($response)) {
-        return $response->get_error_message();
+        throw new Exception($response->get_error_message());
     }
 
     $token = json_decode($response['body']);
@@ -187,6 +307,126 @@ function get_access_token($options) {
     return $token;
 }
 
+function create_contact($options, $url, $user) {
+    $access_token = get_access_token($options);
+
+    // Do nothing if the access token is not ready
+    if (empty($access_token)) {
+        return;
+    }
+
+    $body = array(
+        'data' => array(
+            'type' => 'Contacts',
+            'attributes' => array(
+                'first_name' => $user->first_name,
+                'last_name' => $user->last_name,
+                'email1' => $user->user_email
+            )
+        )
+    );
+    
+    // Create a new contact
+    $response = wp_remote_post($url, array(
+        'headers' => array(
+            'content-type' => 'application/json',
+            'authorization' => sprintf('Bearer %s', $access_token)
+        ),
+        'body' => wp_json_encode($body)
+    ));
+
+    if (is_wp_error($response)) {
+        throw new Exception($response->get_error_message());
+    }
+
+    $responseJson = json_decode($response['body']);
+    return $responseJson->data;
+}
+
+function create_external_id($options, $url, $user, $userContactId) {
+    $access_token = get_access_token($options);
+
+    // Do nothing if the access token is not ready
+    if (empty($access_token)) {
+        return;
+    }
+
+    $externalIdModule = '';
+
+    $system_name = '';
+    $system_name_attr = '';
+    $system_username_attr = '';
+    $system_user_id_attr = '';
+    $contact_id_attr = '';
+
+    if(array_key_exists('onboard_suitecrm_field_external_id_module', $options)) {
+        $externalIdModule = $options['onboard_suitecrm_field_external_id_module'];
+    } else {
+        // Do nothing if the external Id module is not set
+        return;
+    }
+
+    if(array_key_exists('onboard_suitecrm_field_system_name', $options)) {
+        $system_name = $options['onboard_suitecrm_field_system_name'];
+    } else {
+        // Do nothing if the external system name is not set
+        return;
+    }
+
+    if(array_key_exists('onboard_suitecrm_field_attribute_system_name', $options)) {
+        $system_name_attr = $options['onboard_suitecrm_field_attribute_system_name'];
+    } else {
+        // Do nothing if the external system name is not set
+        return;
+    }
+
+    if(array_key_exists('onboard_suitecrm_field_attribute_username', $options)) {
+        $system_username_attr = $options['onboard_suitecrm_field_attribute_username'];
+    }
+
+    if(array_key_exists('onboard_suitecrm_field_attribute_user_id', $options)) {
+        $system_user_id_attr = $options['onboard_suitecrm_field_attribute_user_id'];
+    } else {
+        // Do nothing if the external system user id is not set
+        return;
+    }
+
+    if(array_key_exists('onboard_suitecrm_field_attribute_contact_id', $options)) {
+        $contact_id_attr = $options['onboard_suitecrm_field_attribute_contact_id'];
+    } else {
+        // Do nothing if the external system user id is not set
+        return;
+    }
+
+    $body = array(
+        'data' => array(
+            'type' => $externalIdModule,
+            'attributes' => array(
+                $system_name_attr => $system_name,
+                $system_username_attr => $user->user_login,
+                $system_user_id_attr => $user->ID,
+                $contact_id_attr => $userContactId
+            )
+        )
+    );
+    
+    // Create external Id
+    $response = wp_remote_post($url, array(
+        'headers' => array(
+            'content-type' => 'application/json',
+            'authorization' => sprintf('Bearer %s', $access_token)
+        ),
+        'body' => wp_json_encode($body)
+    ));
+
+    if (is_wp_error($response)) {
+        throw new Exception($response->get_error_message());
+    }
+
+    $responseJson = json_decode($response['body']);
+    return $responseJson->data;
+}
+
  /**
   * Create a contact for the registered user in SuiteCRM
   *
@@ -196,9 +436,6 @@ function onboard_user($user_id) {
     $user = get_userdata($user_id);
 
     if ($user) {
-        $options = get_option('onboard_options');
-        $access_token = get_access_token($options);
-
         $urlBase = '';
 
         if(array_key_exists('onboard_suitecrm_field_urlbase', $options)) {
@@ -207,29 +444,11 @@ function onboard_user($user_id) {
             return;
         }
 
-        $url =sprintf('%s/V8/module', $urlBase);
-        $body = array(
-            'data' => array(
-                'type' => 'Contacts',
-                'attributes' => array(
-                    'first_name' => $user->first_name,
-                    'last_name' => $user->last_name,
-                    'email1' => $user->user_email
-                )
-            )
-        );
-        // Create a new contact
-        $response = wp_remote_post($url, array(
-            'headers' => array(
-                'content-type' => 'application/json',
-                'authorization' => sprintf('Bearer %s', $access_token)
-            ),
-            'body' => wp_json_encode($body)
-        ));
+        $options = get_option('onboard_options');
+        $url = sprintf('%s/V8/module', $urlBase);
 
-        if (is_wp_error($response)) {
-            throw new Exception($response->get_error_message());
-        }
+        $contact = create_contact($options, $url, $user);
+        create_external_id($options, $url, $user, $contact->id);
     }
 }
 
